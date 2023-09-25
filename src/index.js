29a6320745a9cc7605e4cecfe055ef66dcb2c633
Enki Layman <layman212@gmail.com>
2023-09-25 05:09:52 -0400

    added page routing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/home-page/Home';
+import About from './pages/about-page/About';
+import Portfolio from './pages/portfolio-page/Portfolio';
+import Contact from './pages/contact-page/Contact.js';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/about",
+    element: <About/>,
+  },
+  {
+    path: "/portfolio",
+    element: <Portfolio/>,
+  },
+  {
+    path: "/contact",
+    element: <Contact/>,
+  },
+]);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
